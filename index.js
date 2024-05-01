0aed945c2b17fc8ae87cea793b9f0a22dd313646
douglasbottan <52838107+douglasbottan@users.noreply.github.com>
2024-05-01 15:02:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730, 440, 570, 2073850];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
